c72a898d2ba97500ff8f6a42b89951d3267798aa
Amanda Alliegro <amanda.m.arnaut@gmail.com>
2024-08-28 22:34:30 -0400

    adding new component, finishing header

--- src/app/modules/side-bar/side-bar.component.ts
+++ src/app/modules/side-bar/side-bar.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
-
+import { faChessQueen, faComments, faEnvelope, faLayerGroup } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-side-bar',
   templateUrl: './side-bar.component.html',
   styleUrls: ['./side-bar.component.sass']
 })
 export class SideBarComponent {
+  sections = [
+    {
+      icon: faChessQueen,
+      title: "About Me",
+      link: ""
+    },
+    {
+      icon: faComments,
+      title: "Recommendations",
+      link: ""
+    },
+    {
+      icon: faLayerGroup,
+      title: "Technologies",
+      link: ""
+    },
+    {
+      icon: faEnvelope,
+      title: "Contact",
+      link: ""
+    }
 
+  ]
 }
