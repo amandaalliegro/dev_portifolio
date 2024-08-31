21fb0aade67f7f7f58cf62e672348cc63344afd4
Amanda Alliegro <amanda.m.arnaut@gmail.com>
2024-08-31 14:08:36 -0400

    finishing landing page

--- src/app/modules/about-me/about-me.component.ts
+++ src/app/modules/about-me/about-me.component.ts
@@ -6,5 +6,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./about-me.component.sass']
 })
 export class AboutMeComponent {
-
+  frontStacks = [
+    {
+      stack: "Angular",
+      bar: "80%"
+    },
+    {
+      stack: "React",
+      bar: "40%"
+    },
+    {
+      stack: "TypeScript",
+      bar: "85%"
+    },
+    {
+      stack: "SCSS/CSS3/Sass",
+      bar: "80%"
+    },
+    {
+      stack: "HTML5",
+      bar: "90%"
+    },
+    {
+      stack: "JavaScript",
+      bar: "75%"
+    },
+    {
+      stack: "Bootstrap",
+      bar: "70%"
+    }
+  ]
+  backStacks = [
+    {
+      stack:"Node.js",
+      bar: "50%"
+    },
+    {
+      stack:"Express.js",
+      bar: "50%"
+    },
+    {
+      stack:"PostgreSQL",
+      bar: "30%"
+    },
+  ]
 }
